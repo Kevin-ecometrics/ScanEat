4ba39633b84754f94232973fdacd645e0a01a3c2
Kevin-ecometrics <kevin@e-commetrics.com>
2026-06-10 12:11:03 -0700

    update bento

--- app/components/Gallery.tsx
+++ app/components/Gallery.tsx
@@ -1,31 +1,36 @@
 "use client";
 
-import { motion, AnimatePresence } from "framer-motion";
-import { RevealList } from "./Reveal";
+import { Reveal } from "./Reveal";
 
 export default function Gallery() {
   return (
     <section id="gallery" className="px-6 bg-white">
       <div className="max-w-7xl mx-auto">
-        <RevealList stagger={0.1} className="grid grid-cols-2 gap-4">
-          <img
-            src="/agiliza_servicio.jpg"
-            alt=""
-            className="rounded-2xl row-start-1 col-start-1"
-          />
+        <div className="grid grid-cols-2 gap-4">
+          <Reveal className="row-start-1 col-start-1">
+            <img
+              src="/agiliza_servicio.jpg"
+              alt=""
+              className="rounded-2xl w-full h-full object-cover"
+            />
+          </Reveal>
 
-          <img
-            src="/mas_restaurantes.jpg"
-            alt=""
-            className="rounded-2xl row-start-2 col-start-1"
-          />
+          <Reveal delay={0.1} className="row-start-2 col-start-1">
+            <img
+              src="/mas_restaurantes.jpg"
+              alt=""
+              className="rounded-2xl w-full h-full object-cover"
+            />
+          </Reveal>
 
-          <img
-            src="/varias_personas.jpg"
-            alt=""
-            className="rounded-2xl row-span-2 col-start-2 row-start-1"
-          />
-        </RevealList>
+          <Reveal delay={0.2} className="row-start-1 row-span-2 col-start-2">
+            <img
+              src="/varias_personas.jpg"
+              alt=""
+              className="rounded-2xl w-full h-full object-cover"
+            />
+          </Reveal>
+        </div>
       </div>
     </section>
   );
